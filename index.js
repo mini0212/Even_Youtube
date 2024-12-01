87a56d36086757b2ee0bef246277002bc077192f
seongmin <min@seongminui-MacBookPro.local>
2024-12-01 23:35:44 +0900

    feat: 제일 작은 사이즈일 때 미디어 쿼리 추가

--- index.js
+++ index.js
@@ -21,24 +21,55 @@ searchButton.addEventListener('click', () => {
 
 // 화면 크기 감지 및 상태 조정
 function handleResize() {
-	const isSmallScreen = window.matchMedia('(max-width: 656px)').matches;
-	const isLargeScreen = window.matchMedia('(min-width: 657px)').matches;
+	const screen428 = window.matchMedia('(max-width: 428px)').matches;
+	const screen656 = window.matchMedia(
+		'(min-width: 429px) and (max-width: 656px)'
+	).matches;
+	const screen657 = window.matchMedia('(min-width: 657px)').matches;
 
-	if (isSearchClicked && isSmallScreen) {
-		return;
+	if (screen657) {
+		isSearchClicked = false;
 	}
 
-	if (isSmallScreen) {
-		// Small screen 상태
+	if (isSearchClicked) {
+		if (screen428) {
+			rightSectionButtons.forEach((button) => {
+				button.style.display = 'none';
+			});
+		} else if (screen656) {
+			rightSectionButtons.forEach((button) => {
+				if (button.id === 'voice-search-button') {
+					button.style.display = 'flex';
+				} else {
+					button.style.display = 'none';
+				}
+			});
+		}
+		return;
+	}
+	console.log(screen428, screen656, screen657);
+	if (screen428) {
+		leftSection[0].style.display = 'flex';
+		middleSection[0].style.display = 'none';
+		middleVoiceButton.style.display = 'flex';
+		rightSectionButtons.forEach((button) => {
+			if (
+				button.id === 'voice-search-button' ||
+				button.id === 'notification-button'
+			) {
+				button.style.display = 'none';
+			} else {
+				button.style.display = 'flex';
+			}
+		});
+	} else if (screen656) {
 		leftSection[0].style.display = 'flex';
 		middleSection[0].style.display = 'none';
 		middleVoiceButton.style.display = 'flex';
 		rightSectionButtons.forEach((button) => {
 			button.style.display = 'flex';
 		});
-	} else if (isLargeScreen) {
-		// Large screen 상태
-		isSearchClicked = false;
+	} else if (screen657) {
 		leftSection[0].style.display = 'flex';
 		middleSection[0].style.display = 'flex';
 		middleVoiceButton.style.display = 'flex';
