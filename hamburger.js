9214958a6095b8318d7ce00d839589a6cfe457a4
nahyun <knahyuk@gmail.com>
2024-12-04 13:55:57 +0900

    스크롤 문제, 화면 문제 해결

--- hamburger.js
+++ hamburger.js
@@ -1,8 +1,22 @@
 const headerMenuButton = document.getElementById('header-menu');
-const sideHeaderMenuButton = document.getElementById('side-header-menu');
 const evenSidebar = document.querySelector('.even-sidebar');
+const evenSidebarHeader = document.querySelector('.even-sidebar-header');
+const sideHeaderMenuButton = document.getElementById('side-header-menu');
 const evenMiniSidebar = document.querySelector('.even-mini-sidebar');
 const mainContainer = document.querySelector('main');
+
+
+// 사이드바 위에 마우스가 있을/없을 때 스크롤 비활성화/활성화
+evenSidebar.addEventListener('mouseenter', () => {
+	evenSidebar.style.overflowY = 'auto'; 
+	evenSidebar.style.overflowX = 'hidden';
+});
+
+evenSidebar.addEventListener('mouseleave', () => {
+	evenSidebar.style.overflowY = 'hidden'; 
+	evenSidebar.style.overflowX = 'hidden';
+});
+
 
 // 화면의 가로 너비가 1312px 이하인지 확인하는 미디어 쿼리
 const mediaQueryLarge = window.matchMedia('(min-width: 1313px)');
@@ -16,6 +30,7 @@ function initializeSidebar() {
         // 큰 화면일 때 초기 상태 설정
         sidebarState = 'large';
         evenSidebar.style.display = 'block';
+				evenSidebar.style.left = '0';
         evenMiniSidebar.style.display = 'none';
         mainContainer.style.marginLeft = '240px';
 				evenSidebar.classList.remove('overlay');
@@ -39,9 +54,14 @@ function initializeSidebar() {
 
 initializeSidebar();
 
+
 // 미디어 쿼리 변경 시에도 초기화
-mediaQueryLarge.addListener(initializeSidebar);
-mediaQueryMedium.addListener(initializeSidebar);
+mediaQueryLarge.addListener(() => {
+	initializeSidebar();
+});
+mediaQueryMedium.addListener(() => {
+	initializeSidebar();
+});
 
 function handleSidebarToggle() {
     if (mediaQueryLarge.matches) {
@@ -56,6 +76,9 @@ function handleSidebarToggle() {
             // 미니 사이드바에서 큰 사이드바로 변경
             evenMiniSidebar.style.display = 'none';
             evenSidebar.style.display = 'block';
+						evenSidebar.style.left = '0';
+            // evenSidebar.style.position = 'relative';
+            evenSidebar.style.overflowX = 'hidden';
             mainContainer.style.marginLeft = '240px';
 						evenSidebar.classList.remove('overlay');
             sidebarState = 'large';
@@ -68,6 +91,8 @@ function handleSidebarToggle() {
 					evenSidebar.style.display = 'block';
 					evenSidebar.classList.add('overlay');
 					evenMiniSidebar.style.display = 'none';
+					evenSidebar.style.position = 'fixed';
+					evenSidebar.style.overflowX = 'hidden';
 
 					// 화면 밖에서 슬라이드 인
 					setTimeout(() => {
@@ -108,6 +133,17 @@ function hideOverlaySidebar() {
 			headerMenuButton.classList.remove('side-header__effect-round-button');
 		}, 150); 
 }
+
+// 사이드바 헤더 위에 마우스가 있을/없을 때 스크롤 비활성화/활성화
+evenSidebarHeader.addEventListener('mouseenter', () => {
+	evenSidebar.style.overflow = 'hidden';
+});
+
+evenSidebarHeader.addEventListener('mouseleave', () => {
+	evenSidebar.style.overflow = 'auto';
+});
+
+
 
 
 // 햄버거 버튼에 클릭 이벤트 리스너 추가
